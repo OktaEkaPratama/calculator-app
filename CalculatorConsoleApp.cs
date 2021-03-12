3f3f160bcb1bb205bcb7d93cd5780c30a0dcc3df
OktaEkaPratama <oktapratama@students.amikom.ac.id>
2021-03-12 22:39:54 +0700

    initial commit

---
+++ CalculatorConsoleApp.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace CalculatorConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool exit = false;
+            while (!exit)
+            {
+                try
+                {
+                    Console.WriteLine("Enter '+' untuk Penambahan");
+                    Console.WriteLine("Enter '-' untuk Pengurangan");
+                    Console.WriteLine("Enter '*' untuk Perkalian");
+                    Console.WriteLine("Enter '/' untuk Pembagian");
+                    Console.WriteLine("Enter 'C' atau 'c' untuk clear");
+                    Console.WriteLine("Enter 'E' atau 'e' untuk exit ");
+                    char read = Convert.ToChar(Console.ReadLine());
+
+                    if (read == '+')
+                    {
+                        Console.WriteLine("Enter angka pertama: ");
+                        double num1 = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine("Enter angka kedua: ");
+                        double num2 = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine($"Jawaban: {num1 + num2}");
+                    }
+                    else if (read == '-')
+                    {
+                        Console.WriteLine("Enter angka pertama: ");
+                        double num1 = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine("Enter angka kedua: ");
+                        double num2 = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine($"Jawaban: {num1 - num2}");
+                    }
+                    else if (read == '*')
+                    {
+                        Console.WriteLine("Enter angka pertama: ");
+                        double num1 = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine("Enter angka kedua: ");
+                        double num2 = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine($"Jawaban: {num1 * num2}");
+                    }
+                    else if (read == '/')
+                    {
+                        Console.WriteLine("Enter angka pertama: ");
+                        double num1 = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine("Enter angka kedua: ");
+                        double num2 = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine($"Jawaban: {num1 / num2}");
+                    }
+                    else if (read == 'C' || read == 'c')
+                    {
+                        Console.Clear();
+                    }
+                    else if (read == 'E' || read == 'e')
+                    {
+                        exit = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid Input");
+                    }
+                }
+                catch { Console.WriteLine("Invalid Input"); }
+            }
+        }
+    }
+}
